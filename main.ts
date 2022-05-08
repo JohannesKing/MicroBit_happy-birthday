b63a60b3dbba3a8f2371eb576f70fbd291a0600b
JohannesKing <105158760+JohannesKing@users.noreply.github.com>
2022-05-08 16:54:20 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 1; index++) {
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Quarter))
+    }
+    for (let index = 0; index < 1; index++) {
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Quarter))
+    }
+    for (let index = 0; index < 1; index++) {
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Quarter))
+    }
+    for (let index = 0; index < 1; index++) {
+        music.playTone(466, music.beat(BeatFraction.Half))
+        music.playTone(466, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.playTone(466, music.beat(BeatFraction.Quarter))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Double))
+    }
+})
